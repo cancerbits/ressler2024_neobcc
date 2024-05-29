f12bb751ea4d5835dc959bcb11bd6bdbb3b956fe
maud-p <100755946+maud-p@users.noreply.github.com>
2024-05-29 17:25:33 +0200

    Create get_ref_human_pbmc.sh

---
+++ bash/get_ref_human_pbmc.sh
@@ -0,0 +1,3 @@
+wget https://zenodo.org/record/4546839/files/idx.annoy?download=1 -O idx.annoy
+wget https://zenodo.org/record/4546839/files/ref.Rds?download=1 -O ref.Rds
+echo $'Version: 1.0.0\nDate: February 17, 2021\nDOI: 10.5281/zenodo.4546839\nLink: https://zenodo.org/record/4546839\n' > details.txt
